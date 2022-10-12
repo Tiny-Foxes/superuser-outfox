581881661e6edca78a87e632c3e17480b25627f0
Sudospective <avery@sudospective.net>
2022-10-11 22:02:24 -0700

    Fix judgment loading, weird opaque layer on some grades

--- Modules/Options.SmartJudgments.lua
+++ Modules/Options.SmartJudgments.lua
@@ -1,7 +1,8 @@
 return function(Mode)
 	local Dir = FILEMAN:GetDirListing("Appearance/Judgments/",false,true)
 	-- Include theme judgments as well. ~Sudo
-	for k, v in pairs(FILEMAN:GetDirListing(THEME:GetCurrentThemeDirectory()..'Judgments/', false, true)) do
+	local ThemeDir = FILEMAN:GetDirListing(THEME:GetCurrentThemeDirectory()..'Judgments/', false, true)
+	for k, v in pairs(ThemeDir) do
 		Dir[k] = v
 	end
 	local NewDir = {}
